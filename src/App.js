d1119427ec3f2fd980694ca886b43e04e6e39a2f
Elsa <elsashaikhpk@gmail.com>
2023-08-25 17:06:29 +0500

    TextUtils

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Aboutus from './components/Aboutus';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 
+
+
+// let name = "Elsa Shaikh";
 function App() {
+ const [mode, setMode] = useState('light');
+ const [alert, setAlert] = useState(null);
+
+ const showAlert= (message,type)=>{
+        setAlert({
+          msg:message,
+          type:type
+        })
+        setTimeout(()=>{
+          setAlert(null);
+        },3000);
+ }
+
+ const enableMode = ()=>{
+  if(mode === 'light'){
+  setMode('dark')
+  document.body.style.backgroundColor='#042743';
+  showAlert("Dark Mode Enable","success");
+  document.title = "TextUtils - Dark Mode";
+//   setInterval(() => {
+//     document.title = "TextUtils is Amazing";
+//   }, 2000);
+// setInterval(() => {
+//   document.title = "Install TextUtils Now"; 
+// }, 1500);
+
+}else{
+  setMode('light')
+  document.body.style.backgroundColor='white'
+  showAlert("Light Mode Enable","success");
+  document.title = "TextUtils - Light Mode";
+}
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+ <Router>
+  <Navbar title="TextUtils" aboutText = "About Us" mode={mode} enableMode={enableMode}/>
+  <Alert alert={alert}/>
+    <Routes>
+      <Route exact path="/about" element={<Aboutus/>}></Route>
+      <Route exact path="/" element={<TextForm heading= "Enter the text to analyze below" mode={mode} showAlert= {showAlert}/>
+}></Route>
+    </Routes>
+  </Router>
+   </>
   );
 }
 
